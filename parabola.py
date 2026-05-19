ef4eec9c4f00dd72bcd532991661463141d6816a
Ng Lorin Theodore <lorin@Ngs-MacBook-Air.local>
2026-05-19 17:30:40 +0800

    parabola.py

--- parabola.py
+++ parabola.py
@@ -1,1 +1,16 @@
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 100)
+y = x ** 2
+plt.plot(x, y, label="y = x^2", color="black", linewidth=2)
+plt.title("y=x^2")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid(True)
+plt.legend()
+plt.show()
+
+x = np.linspace(-10, 10, 100)
+y = x ** 2
+
